3e5469ee8e07136691720f1670787fbbfc7df0b9
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2023-11-24 20:27:02 -0300

    add 14 without trie solution

---
+++ data_structures/trees/binary_trees/14/without_trie.go
@@ -0,0 +1,28 @@
+func isPrefixCandidate(cand string, strs []string) bool {
+    n := len(cand)
+    for _, s := range strs {
+        if n > len(s) {
+            return false
+        }
+        if cand != s[:n] {
+            return false
+        } 
+    }
+    return true
+}
+
+func longestCommonPrefix(strs []string) string {
+    longest := ""
+    for _, s := range strs {
+        // l = len(s) because in Go s[:l] is non inclusive
+        for l := len(s); l >= 1; l-- {
+            candidate := s[:l]
+            if isPrefixCandidate(candidate, strs) {
+                if len(candidate) > len(longest) {
+                    longest = candidate
+                }
+            }
+        }
+    }
+    return longest
+}
